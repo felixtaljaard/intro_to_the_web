55e1337befec8bf4164485974fa62018f6ebbb39
Felix Taljaard <felixtaljaard@hotmail.com>
2022-03-07 16:59:42 +0000

    Cat picture added

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
 
 get '/' do
-    "Hello World"
-  end
+    'hello!'
+end
+
+get '/secret' do
+  'secret howdy!'
+end
+
+get '/extra' do
+    "here's an extra little bit"
+end
+
+get '/cat' do
+    "<div style='border: 3px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+    <div>"
+end
