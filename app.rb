634ae02a6346c56fd8169618a5269f70789536c2
Felix Taljaard <felixtaljaard@hotmail.com>
2022-03-07 17:54:39 +0000

    refactored the heavy lifting into controllers

--- app.rb
+++ app.rb
@@ -2,20 +2,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 
-get '/' do
-    'hello!'
-end
-
-get '/secret' do
-  'secret howdy!'
-end
-
-get '/extra' do
-    "here's an extra little bit"
-end
-
 get '/cat' do
-    "<div style='border: 3px dashed red'>
-    <img src='https://i.imgur.com/jFaSxym.png'>
-    <div>"
+    @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
 end
